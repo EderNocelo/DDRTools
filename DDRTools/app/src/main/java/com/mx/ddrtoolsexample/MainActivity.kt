554482893d000a8e88ffc2b101673f6a0916db6b
Eder Gomez Nocelo <gnocelo.eder@gmail.com>
2021-01-13 19:19:09 -0600

    Nueva versión.

--- DDRTools/app/src/main/java/com/mx/ddrtoolsexample/MainActivity.kt
+++ DDRTools/app/src/main/java/com/mx/ddrtoolsexample/MainActivity.kt
@@ -2,10 +2,14 @@ package com.mx.ddrtoolsexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mx.ddrtools.utils.DDRString
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
